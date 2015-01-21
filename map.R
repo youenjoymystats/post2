662b5874f31162dfed780c1518d21e216c0ba8b3
gm758 <info@youenjoymystats.com>
2015-01-20 19:22:23 -0500

    final runs

--- map.R
+++ map.R
@@ -3,10 +3,10 @@ cities <- read.csv("cities.csv")
 #missing <- data.frame(state = state.abb[!(state.abb %in% states$state)], count = 0)
 #states <- rbind(states,missing)
 
-names(cities) <- c("latlon","city","count","dates")
+names(cities) <- c("latlon","city","shows","dates")
 cities$col <- rep(0, length(cities$count))
 cities$test <- rep("tesnblah", length(cities$count))
-map <- gvisGeoChart(cities,locationvar = "latlon",  sizevar = "count",
+map <- gvisGeoChart(cities,locationvar = "latlon",  sizevar = "shows",
                     hovervar = "city", options=list(region="US", 
                                 displayMode="markers", 
                                 resolution="provinces",
@@ -18,19 +18,37 @@ map <- gvisGeoChart(cities,locationvar = "latlon",  sizevar = "count",
 print(map,"chart",file="map.txt")
 plot(map)
 
-burlington <- tapply(data$city=="Burlington", data$year, sum)
-nyc <- tapply(data$city=="New York", data$year, sum)
+data <- read.csv("burlington_nyc.csv")
 
-df.burlington <- data.frame(city = "Burlington", shows = burlington, 
-                            year = rownames(burlington))
-df.nyc <- data.frame(city = "NYC", shows = nyc, 
-                            year = rownames(nyc))
+burlington <- tapply(data$City=="Burlington", data$Year, sum)
+nyc <- tapply(data$City=="NYC", data$Year, sum)
+
+years <- seq(1983, 2015, 1)
+
+df.burlington <- data.frame(City = "Burlington", Shows = burlington, 
+                            Year = rownames(burlington), stringsAsFactors = FALSE)
+df.nyc <- data.frame(City = "NYC", Shows = nyc, 
+                            Year = rownames(nyc), stringsAsFactors = FALSE)
+
+missing.burlington <- 
+        data.frame(City = "Burlington", 
+                   Year = years[!(years %in% df.burlington$Year)], Shows = 0, stringsAsFactors = FALSE)
+missing.nyc <- 
+        data.frame(City = "NYC", 
+                   Year = years[!(years %in% df.nyc$Year)], Shows = 0, stringsAsFactors = FALSE)
+
+df.burlington <- rbind(df.burlington, missing.burlington)
+df.nyc <- rbind(df.nyc, missing.nyc)
+
+
 df <- rbind(df.burlington, df.nyc)
-mn <- min(as.numeric(as.character(df$year)))
-mx <- max(as.numeric(as.character(df$year)))
-          
-ggplot(df, aes(year, shows, colour = city)) + 
+mn <- min(as.numeric(as.character(df$Year)))
+mx <- max(as.numeric(as.character(df$Year)))
+
+
+
+ggplot(df, aes(Year, Shows, colour = City)) + 
         geom_point() + 
-        geom_line(aes(group=city)) +
-        scale_x_discrete(breaks=pretty(c(mn,max), n = 10))
-ggsave(file="ggplot.png")
+        geom_line(aes(group=City)) +
+        scale_x_discrete(breaks=pretty(c(mn,mx), n = 10))
+ggsave(file="burlington_nyc.png", width = 3, height = 3)
